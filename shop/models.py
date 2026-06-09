8ee52bc27b72c37a2a1e2e092a1433f47ddab0bd
ADZIKUMA <adzikuma.kevin@gmail.com>
2026-06-09 22:14:40 +0000

    feat: posts creation with images completely working

--- shop/models.py
+++ shop/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
         verbose_name_plural = "Categories"
+
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, null=True, blank=True)
